0e63485d2e28819be9e25f2c771a3825505cb32d
ybon3 <lee.ybon@gmail.com>
2016-09-02 14:26:52 +0800

    Complete Task

--- src/main/java/com/dtc/test/client/Task4.java
+++ src/main/java/com/dtc/test/client/Task4.java
@@ -16,14 +16,14 @@ public class Task4 implements EntryPoint {
 		HorizontalLayoutContainer hlcTop = new HorizontalLayoutContainer();
 		hlcTop.add(new TextButton("w:50%, h:50px"), new HorizontalLayoutData(0.5, 50));
 		hlcTop.add(new TextButton("w:50%, h:50px"), new HorizontalLayoutData(0.5, 50));
-
+		
 		HorizontalLayoutContainer hlcBottomRightBottom = new HorizontalLayoutContainer();
 		hlcBottomRightBottom.add(new TextButton("w:200px"), new HorizontalLayoutData(200, 1.0));
 		hlcBottomRightBottom.add(new TextButton("?"), new HorizontalLayoutData(1.0, 1.0));
 		hlcBottomRightBottom.add(new TextButton("w:80px h:80px"), new HorizontalLayoutData(80, 80));
 		
 		VerticalLayoutContainer vlcBottomRight = new VerticalLayoutContainer();
-		vlcBottomRight.add(new TextButton("h:20%(不含上頭的50px)"), new VerticalLayoutData(1.0, 0.2));
+		vlcBottomRight.add(new TextButton("h:20%(不含上頭的 50px)"), new VerticalLayoutData(1.0, 0.2));
 		vlcBottomRight.add(hlcBottomRightBottom, new VerticalLayoutData(1.0, 0.8));
 		
 		HorizontalLayoutContainer hlcBottom = new HorizontalLayoutContainer();
@@ -34,10 +34,10 @@ public class Task4 implements EntryPoint {
 		vlcMain.add(hlcTop, new VerticalLayoutData(1.0, 50));
 		vlcMain.add(hlcBottom, new VerticalLayoutData(1.0, 1.0));
 		
-	    Viewport vp = new Viewport();
-	    vp.add(vlcMain);
+		Viewport vp = new Viewport();
+		vp.add(vlcMain);
 
-	    RootPanel.get().add(vp);
+		RootPanel.get().add(vp);
 		
 	}
 }
